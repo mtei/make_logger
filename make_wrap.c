056314cac8d964c07f0a3a6251345e03bb81b26f
ISII takesi <isii@harmony.co.jp>
2012-08-12 02:40:21 +0900

    make_wrap.c: log にカレントディレクトリの表示追加

--- make_wrap.c
+++ make_wrap.c
@@ -23,38 +23,43 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void print_args(struct timeval *timev, pid_t pid, int exitcode, int argc, char *argv[]);
+void print_args(struct timeval *timev, pid_t ppid, pid_t pid, int exitcode,
+		const char *cwd, int argc, char *argv[]);
+
+char cwdbuf[1024];
 
 int main(int argc, char *argv[])
 {
     struct timeval stimev;
     struct timeval etimev;
-
     int r;
-    pid_t p, wp;
+    pid_t thisp, subp, ppid, wp;
     const char *cmd;
-    
+    const char *cwd;
+
     cmd = getenv("MAKE_WRAP");
     if( cmd == NULL )
 	cmd = "make";
+    ppid = getppid();
+    thisp = getpid();
+    getcwd(cwdbuf, sizeof(cwdbuf));
+    cwd = strrchr(cwdbuf, '/')+1;
     gettimeofday(&stimev,NULL);
-    p = fork();
-    if( p == 0 ) {
+
+    subp = fork();
+    if( subp == 0 ) {
 	execvp(cmd, argv);
     }
+    print_args(&stimev, ppid, subp, -1, cwd, argc, argv);
     wp = wait(&r);
     gettimeofday(&etimev,NULL);
-    if( wp < 0 ) {
-	perror(argv[0]);
+    if( wp < 0 ) { perror(argv[0]); exit(1); }
+    if( subp != wp ) {
+	fprintf(stderr, "make_wrap %s: pid %d %d unmatch\n", argv[0], subp, wp);
 	exit(1);
     }
-    if( p != wp ) {
-	fprintf(stderr, "make_wrap %s: pid %d %d unmatch\n", argv[0], p, wp);
-	exit(1);
-    }
-    print_args(&stimev, p, -1, argc, argv);
     if( WIFEXITED(r) ) {
-	print_args(&etimev, p, WEXITSTATUS(r), argc, argv);
+	print_args(&etimev, ppid, subp, WEXITSTATUS(r), cwd, argc, argv);
 	exit(WEXITSTATUS(r));
     } else if (WIFSIGNALED(r)) {
 	fprintf(stderr, "make_wrap %s signal %d\n", argv[0], WTERMSIG(r));
@@ -64,10 +69,11 @@ int main(int argc, char *argv[])
 
 /*
  * print format
- *  ppid,pid: time {start,exit(code)} : argv[0] argv[1] ...
+ *  ppid,pid: time {start,exit(code)} : cwd/ argv[0] argv[1] ...
  *
  */
-void print_args(struct timeval *timev, pid_t pid, int exitcode, int argc, char *argv[])
+void print_args(struct timeval *timev, pid_t ppid, pid_t pid, int exitcode,
+		const char *cwd, int argc, char *argv[])
 {
     struct tm *rlc;
     int i;
@@ -82,13 +88,14 @@ void print_args(struct timeval *timev, pid_t pid, int exitcode, int argc, char *
 	    fp = stdout;
     }
     rlc = gmtime(&(timev->tv_sec));
-    fprintf(fp, "%3d,%3d: %02d:%02d:%02d.%03d ", getppid(), pid,
+    fprintf(fp, "%3d,%3d: %02d:%02d:%02d.%03d ", ppid, pid,
 	    rlc->tm_hour, rlc->tm_min, rlc->tm_sec,
 	    timev->tv_usec/1000);
     if( exitcode < 0 )
 	fprintf(fp, "start   : ");
     else
 	fprintf(fp, "exit(%d) : ",exitcode);
+    fprintf(fp,"%s/  ", cwd);
     for( i = 0; i < argc; i ++ )
 	fprintf(fp, "%s%c", argv[i], (i + 1 < argc)?' ':'\n');
     if( fp != stdout )
