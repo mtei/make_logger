d1a9ab034ddfb6d985e1feeb291115f05a8325db
ISII takesi <isii@harmony.co.jp>
2012-08-13 22:46:11 +0900

    make_wrap.c: 実行時刻だけではなく実行時間も表示するように変更

--- make_wrap.c
+++ make_wrap.c
@@ -8,6 +8,10 @@
  *     $ make_wrap <make command arguments>...
  *     $ cat logfile.txt
  *
+ *  environment variable
+ *     MAKE_WRAP_LOG  -- log file name
+ *     MAKE_WRAP      -- make command name
+ *
  *      Date      Design                     Log
  *  ------------  -------------------------  --------------------
  *   2012-08-11   isii@Harmony Systems       created
@@ -23,8 +27,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void print_args(struct timeval *timev, pid_t ppid, pid_t pid, int exitcode,
-		int mlevel, const char *cwd, int argc, char *argv[]);
+void print_args(struct timeval *stimev, struct timeval *etimev,
+		pid_t ppid, pid_t pid, int exitcode,int mlevel, const char *cwd,
+		int argc, char *argv[]);
 
 char cwdbuf[1024];
 
@@ -56,7 +61,7 @@ int main(int argc, char *argv[])
     if( subp == 0 ) {
 	execvp(cmd, argv);
     }
-    print_args(&stimev, ppid, subp, -1, mlevel, cwd, argc, argv);
+    print_args(&stimev, &stimev, ppid, subp, -1, mlevel, cwd, argc, argv);
     wp = wait(&r);
     gettimeofday(&etimev,NULL);
     if( wp < 0 ) { perror(argv[0]); exit(1); }
@@ -65,7 +70,7 @@ int main(int argc, char *argv[])
 	exit(1);
     }
     if( WIFEXITED(r) ) {
-	print_args(&etimev, ppid, subp, WEXITSTATUS(r), mlevel, cwd, argc, argv);
+	print_args(&stimev, &etimev, ppid, subp, WEXITSTATUS(r), mlevel, cwd, argc, argv);
 	exit(WEXITSTATUS(r));
     } else if (WIFSIGNALED(r)) {
 	fprintf(stderr, "make_wrap %s signal %d\n", argv[0], WTERMSIG(r));
@@ -75,16 +80,18 @@ int main(int argc, char *argv[])
 
 /*
  * print format
- *  ppid,pid: time {start,exit(code)} : cwd/ argv[0] argv[1] ...
+ *  ppid,pid: clock time (nest) {start,exit(code)} : cwd/ argv[0] argv[1] ...
  *
  */
-void print_args(struct timeval *timev, pid_t ppid, pid_t pid, int exitcode,
-		int mlevel, const char *cwd, int argc, char *argv[])
+void print_args(struct timeval *stimev, struct timeval *etimev,
+		pid_t ppid, pid_t pid, int exitcode,int mlevel, const char *cwd,
+		int argc, char *argv[])
 {
     struct tm *rlc;
     int i;
     const char *logfile;
     FILE *fp;
+    struct timeval dtimev;
     
     fp = stdout;
     logfile = getenv("MAKE_WRAP_LOG");
@@ -93,10 +100,20 @@ void print_args(struct timeval *timev, pid_t ppid, pid_t pid, int exitcode,
 	if( fp == NULL )
 	    fp = stdout;
     }
-    rlc = gmtime(&(timev->tv_sec));
-    fprintf(fp, "%3d,%3d: %02d:%02d:%02d.%03d (%d) ", ppid, pid,
+    rlc = gmtime(&(etimev->tv_sec));
+    fprintf(fp, "%3d,%3d: %02d:%02d:%02d.%03d ", ppid, pid,
 	    rlc->tm_hour, rlc->tm_min, rlc->tm_sec,
-	    timev->tv_usec/1000, mlevel);
+	    etimev->tv_usec/1000);
+    dtimev.tv_sec = etimev->tv_sec - stimev->tv_sec;
+    dtimev.tv_usec = etimev->tv_usec - stimev->tv_usec;
+    if( dtimev.tv_usec < 0 ) {
+	dtimev.tv_usec += 1000000;
+	dtimev.tv_sec --;
+    }
+    rlc = gmtime(&(dtimev.tv_sec));
+    fprintf(fp, "%2d:%02d:%02d.%03d (%d) ",
+	    rlc->tm_hour, rlc->tm_min, rlc->tm_sec,
+	    dtimev.tv_usec/1000, mlevel);
     if( exitcode < 0 )
 	fprintf(fp, "start   : ");
     else
